0531ba307c47430d85ce1fcb9c6d40a4035d3427
Siddhartha Dikshit <nivogroup@gmail.com>
2025-07-31 11:46:19 +0530

    User and ssl

--- API/Program.cs
+++ API/Program.cs
@@ -9,11 +9,14 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
+//Configure the HTTP request pipeline
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
